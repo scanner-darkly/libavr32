2aa3e4f0e4b4dded3203237591b92875428c6ad0
scanner-darkly <scannerdarkly.git@gmail.com>
2017-05-24 20:32:51 -0700

    update irqs_pause | irqs_resume (to not use a global var and to not block i2c level, hopefully)

--- conf/conf_tc_irq.c
+++ conf/conf_tc_irq.c
@@ -1,12 +1,13 @@
 #include "interrupt.h"
 #include "conf_tc_irq.h"
 
-static volatile irqflags_t irq_flags;
-
-void irqs_pause( void ) {
-  irq_flags = cpu_irq_save();
+irqflags_t irqs_pause( void ) {
+  irqflags_t irq_flags = sysreg_read(AVR32_SR);
+  cpu_irq_disable_level(UI_IRQ_PRIORITY);
+  cpu_irq_disable_level(APP_TC_IRQ_PRIORITY);
+  return irq_flags;
 }
 
-void irqs_resume( void ) {
+void irqs_resume( irqflags_t irq_flags ) {
   cpu_irq_restore(irq_flags);
 }
